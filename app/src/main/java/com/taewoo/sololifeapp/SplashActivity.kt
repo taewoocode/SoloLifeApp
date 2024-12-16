6bfd2476eff2f60a5576132d3659899ebfd344c3
taewoocode <taewoocode@gmail.com>
2024-12-16 23:32:24 +0900

    인트로 페이지, 스플래시 페이지

--- app/src/main/java/com/taewoo/sololifeapp/SplashActivity.kt
+++ app/src/main/java/com/taewoo/sololifeapp/SplashActivity.kt
@@ -1,14 +1,24 @@
 package com.taewoo.sololifeapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.taewoo.sololifeapp.auth.IntroActivity
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        /** 3초뒤 splash 화면으로 이동 **/
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 3000)
+
     }
 }
