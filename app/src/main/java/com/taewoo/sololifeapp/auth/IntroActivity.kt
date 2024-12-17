4bf0ac490f4cfb9ca78a38806729d5a07f9982c0
taewoocode <taewoocode@gmail.com>
2024-12-17 17:56:23 +0900

    비회원 로그인 추가

--- app/src/main/java/com/taewoo/sololifeapp/auth/IntroActivity.kt
+++ app/src/main/java/com/taewoo/sololifeapp/auth/IntroActivity.kt
@@ -4,20 +4,30 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Adapter
 import android.widget.AdapterView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.taewoo.sololifeapp.MainActivity
 import com.taewoo.sololifeapp.R
 import com.taewoo.sololifeapp.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var auth : FirebaseAuth
+
     private lateinit var binding : ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
 //        setContentView(R.layout.activity_intro)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
@@ -30,10 +40,24 @@ class IntroActivity : AppCompatActivity() {
         /** joinBtn 클릭시 이동 **/
         binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
-             startActivity(intent)
+            startActivity(intent)
         }
 
+        /** noAccountBtn **/
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
 
+                        /** 로그인 성공시 매인액티비티로 이동 **/
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        Toast.makeText(this, "login success", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "login false", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
+        }
     }
 }
