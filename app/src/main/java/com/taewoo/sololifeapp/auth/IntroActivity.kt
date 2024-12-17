2534df4fd6cf89c8114d8a84cb1773c666360e20
taewoocode <taewoocode@gmail.com>
2024-12-17 10:16:46 +0900

    아이디, 비밀번호 입력 추가

--- app/src/main/java/com/taewoo/sololifeapp/auth/IntroActivity.kt
+++ app/src/main/java/com/taewoo/sololifeapp/auth/IntroActivity.kt
@@ -1,15 +1,24 @@
 package com.taewoo.sololifeapp.auth
 
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.AdapterView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.taewoo.sololifeapp.R
+import com.taewoo.sololifeapp.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+//        setContentView(R.layout.activity_intro)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
     }
 }
