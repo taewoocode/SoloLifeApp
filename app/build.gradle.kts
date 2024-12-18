10251b70abea55071b754c4eba930cee842bd301
taewoocode <qkrxodn6035@gmail.com>
2024-12-19 00:50:07 +0900

    홈프래그먼트 tipTap Toast 메시지 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
