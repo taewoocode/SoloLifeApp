9169e72fe9ed08e638df92d7e002760b79bf37b1
taewoocode <qkrxodn6035@gmail.com>
2024-12-24 11:43:43 +0900

    리사이클뷰 만들기

---
+++ app/src/main/java/com/taewoo/sololifeapp/contentList/ContentListActivity.kt
@@ -0,0 +1,18 @@
+package com.taewoo.sololifeapp.contentList
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.taewoo.sololifeapp.R
+
+class ContentListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_list)
+    }
+
+    /** recyclerView **/
+
+}
